c878359b4bfca104ca9837b7d5d46453e0658251
Matt <matthias.calatroni@epfl.ch>
2025-03-04 11:13:20 +0100

    produit vectoriel et produit scalaire

--- VecteurValent/VecteurV.cc
+++ VecteurValent/VecteurV.cc
@@ -24,7 +24,7 @@ private:
 
     bool compare(Vecteur B,int precision=1e-10) const{ //la precision est par défaut 1e-10 mais je laisse l'opportunité de la modifier
         if (dim==B.vecteur.size()){
-            for (int i(0); i<=dim ; i++){
+            for (int i(0); i<dim ; i++){
 
                 if ( abs(vecteur[i] - B.vecteur[i]) > precision ){return false;} 
                 else {return true;}}}
@@ -42,8 +42,8 @@ private:
             B = A;
             A = C;
         }
-        for (int i(0); i <= dim_A-dim_B ; i++){B.push_back(0);} // on complète la dimension de B en la dimension de A en lui rajoutant des 0. La copie de A et B permet aussi de ne pas modifier les vecteurs originaux
-        for(int i(0); i<=dim_A ;i++){A[i]= A[i]+B[i];}
+        for (int i(0); i < dim_A-dim_B ; i++){B.push_back(0);} // on complète la dimension de B en la dimension de A en lui rajoutant des 0. La copie de A et B permet aussi de ne pas modifier les vecteurs originaux
+        for(int i(0); i<dim_A ;i++){A[i]= A[i]+B[i];}
         Vecteur D;
         D.vecteur = A;
         return D; //je suis vrm pas sur de ces 2 dernières lignes, parce qu'il faut retourner un Vecteur sauf que je dois manipuler les vecteurs (V majuscule = classe et v minuscule = vector contenant les éléments du Vecteur)
@@ -56,7 +56,7 @@ private:
 
     Vecteur oppose() const{
         vector <double> coord_oppose = vecteur;
-        for(int i(0); i<=dim;i++){
+        for(int i(0); i<dim;i++){
             coord_oppose[i] = 0 - coord_oppose[i];
         }
         Vecteur inverse;
@@ -66,10 +66,46 @@ private:
 
     Vecteur mult(double scalaire) const{
         Vecteur multiplie;
-        for(int i = 0; i <= dim;i++){
+        for(int i = 0; i < dim;i++){
             multiplie.vecteur[i] = vecteur[i];
         }
         return multiplie;
+    }
+
+    double prod_scalaire(Vecteur B) const{
+        //création d'un vecteur temporaire pour ne pas modifier les coords du vecteur de cette classe
+        vector <double> vect_temp = vecteur;
+        do{
+            if(dim > B.vecteur.size()){
+                B.vecteur.push_back(0);
+            }else if(dim < B.vecteur.size()){ 
+                vect_temp.push_back(0);
+            }
+        }while(dim != B.vecteur.size());
+        double produit(0);
+        for(int i = 0; i < dim;i++){
+            produit += (vect_temp[i]*B.vecteur[i]);
+        }
+        return produit;
+    }
+
+    Vecteur prod_vect(Vecteur B) const{
+        // si les vecteurs sont de dimension 2 ou moins on les complètes en vecteurs de dimension trois avec des 0 
+        //dans les coords manquantes. Si les dimension sont > 3 ont ne prends que les 3 premières coords
+        vector <double> vect_temp = vecteur;
+        do{
+            if(dim < 3){
+                vect_temp.push_back(0);
+            }else if(B.vecteur.size() < 3){
+                B.vecteur.push_back(0);
+            }
+        }while(dim != 3 && B.vecteur.size() != 3);
+        Vecteur Produit;
+        Produit.vecteur[0] = (vect_temp[2]*B.vecteur[3])-(vect_temp[3]*B.vecteur[2]);
+        Produit.vecteur[1] = (vect_temp[3]*B.vecteur[1])-(vect_temp[1]*B.vecteur[3]);
+        Produit.vecteur[2] = (vect_temp[1]*B.vecteur[2])-(vect_temp[2]*B.vecteur[1]);
+
+        return Produit;
     }
 
 
