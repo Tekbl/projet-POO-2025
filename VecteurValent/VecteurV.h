08d83ae04342cc8f62f967b61c19e70f552841ec
Valent <valentino.lekimpe@epfl.ch>
2025-03-04 10:48:51 +0100

    Supprimé des choses pas nécessaires dans le .h

--- VecteurValent/VecteurV.h
+++ VecteurValent/VecteurV.h
@@ -14,11 +14,5 @@ public:
 
 
 private:
-    vector<double> vecteur; //on utilise un vector car on peut modifier leur taille (dimension) contrairement aux array
-    void affiche() const{for(auto element:vecteur){cout << element << " ";}; cout << endl;}
-    void set_coord(int i,double v){vecteur[i] = v;}
-    double get_coord(int i)const {return vecteur[i];}
-    void augmente(double v){vecteur.push_back(v);}
-    int dim = vecteur.size(); // on fixe n pour eviter de refaire appel à la fonction size() pour chaque opération 
-
+   
 };
