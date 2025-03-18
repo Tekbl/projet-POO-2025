e793cc70097cbbf4ee8c432e378758506c964d03
Matt <matthias.calatroni@epfl.ch>
2025-03-18 20:57:53 +0100

    modifié le .h pour avoir les constructeurs

--- VecteurValent/VecteurV.h
+++ VecteurValent/VecteurV.h
@@ -6,10 +6,19 @@
 class Vecteur{
 
 public:
+
+    //constructeurs
+    Vecteur(unsigned int dimension);
+    Vecteur(double x, double y, double z);
+    Vecteur(const vector<double>& liste_dinit);
+
+    //mise en place et affichage des vecteurs
     void affiche() const;
     void set_coord(int i,double v);
     double get_coord(int i) const;
     void augmente(double v);
+    
+    //opérations sur les vecteurs
     bool compare(Vecteur B, double precision=1e-10) const;
     Vecteur addition(Vecteur X) const;
     Vecteur soustraction(Vecteur X) const;
@@ -20,8 +29,12 @@ public:
     double norme() const;
     double norme2() const;
     Vecteur unitaire() const;
+
+    //opérateurs
     friend ostream& operator<<(ostream& sortie,const Vecteur& v);
     bool operator==(Vecteur B) const;
+    void operator+=(const Vecteur& B);
+    void operator-=(const Vecteur& B);
 
 private:
    
