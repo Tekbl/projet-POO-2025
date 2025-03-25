0719f5ab3dae99d1ecc3be8d8838e0581ba07cf3
Valent <valentino.lekimpe@epfl.ch>
2025-03-25 13:34:25 +0100

    Update GravitationConstante, y'a des error mais jsp pk, je pense que ça va pas impacter la compilation

--- GravitationConstante.cc
+++ GravitationConstante.cc
@@ -10,12 +10,14 @@ using namespace std;
 class GravitationConstante{
 
     public :
-        GravitationConstante():gravitation.vecteur=g{}
+        GravitationConstante():gravitation(g) {}; //constructeur par defaut qui utilise g
         Vecteur force(PointMateriel p, double t) const;
+    
 
     private :
-        Vecteur gravitation();
+        Vecteur gravitation;
 };
 
 
-Vecteur GravitationConstante::force(PointMateriel p, double t) const{return gravitation.vecteur.mult(p.masse);} // F=m*a avec a=champ de grav et m = p.masse et on utilise la méthode mult définie dans la classe vecteur pour les multiplier
+Vecteur GravitationConstante::force(PointMateriel p, double t) const{
+    return gravitation.mult(p.masse);} // F=m*a avec a=champ de grav et m = p.masse et on utilise la méthode mult définie dans la classe vecteur pour les multiplier
--- GravitationConstante.h
+++ GravitationConstante.h
@@ -1,7 +1,20 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include <cmath>
 #include "VecteurV.h"
 #include "PointMateriel.h"
 #include "constantes.h"
 
-Vecteur force(PointMateriel p, double t) const;
-Vecteur gravitation();
+
+class GravitationConstante{
+
+    public :
+        GravitationConstante(); 
+        Vecteur force(PointMateriel p, double t) const;
+    
+
+    private :
+        Vecteur gravitation;
+};
+
