140a8d2591b0c81335a6fea2c9b8b26d2dc905fb
Valent <valentino.lekimpe@epfl.ch>
2025-03-27 11:02:00 +0100

    fait la Méthode de Euler

--- Integrateurs.cc
+++ Integrateurs.cc
@@ -2,27 +2,32 @@
 #include <vector>
 #include <cmath>
 #include "VecteurV.h"
-#include "ObjetIntegrable.h" //a changer/update plus tard
+#include "ObjetIntegrable.h"
 
 
 
 class integrateur{
     public :
-        virtual void evolue(objet_integrable obj, double t, double dt)=0;
+        virtual void evolue(ObjetMobile *obj, double t, double dt)=0;
     
 };
 
 class integrateurEulerCromer : public integrateur{
     public :
-        virtual void evolue(objet_integrable obj, double t, double dt) override
+        virtual void evolue(ObjetMobile *obj, double t, double dt) override;
             
-    private :
-
 };
+//comment on fait la diff entre premier ordre et deuxième ordre ??
+//nom des méthodes a changer
+//pointeurs et ou références à implémenter 
 
-virtual void integrateurEulerCromer::evolue(objet_integrable obj, double t, double dt) override{
-
+void integrateurEulerCromer::evolue(ObjetMobile *obj, double t, double dt) override {
+    obj->set_E_pr(obj->get_E_pr() + obj->evolution(t)*dt); //évolue E_pr
+    obj->set_E(obj->get_E() + obj->get_E_pr()*dt ); //évolue E
 }
+
+class Newmark: public integrateur{}; 
+
 
 class RungeKutta4: public integrateur{}; 
 
