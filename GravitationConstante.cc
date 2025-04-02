5f32c61e7802af5f851747b81f2a9d827e24ae7f
Valent <valentino.lekimpe@epfl.ch>
2025-04-02 11:28:49 +0200

    MàJ de GravitationConstante (héritage de Champforce) + bug fix

--- GravitationConstante.cc
+++ GravitationConstante.cc
@@ -3,21 +3,20 @@
 #include <cmath>
 #include "constantes.h"
 #include "PointMateriel.h"
+#include "Champforces.h"
 #include "VecteurV.h"
 using namespace std;
 
 
-class GravitationConstante{
+class GravitationConstante : ChampForces {
 
     public :
         GravitationConstante():gravitation(g) {}; //constructeur par defaut qui utilise g
        
-
+        Vecteur force(PointMateriel p, double t) const{
+            return gravitation.mult(p.masse);} //F=m*a avec a=champ de grav et m = p.masse et on utilise la méthode mult définie dans la classe vecteur pour les multiplier
     private :
         Vecteur gravitation;
 };
 
 
-Vecteur GravitationConstante::force(PointMateriel p, double t) const{
-    return gravitation.mult(p.masse);} // F=m*a avec a=champ de grav et m = p.masse et on utilise la méthode mult définie dans la classe vecteur pour les multiplier
-//!!! à déplacer dans la classe objetphysique plus tard !!!
