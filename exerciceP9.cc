d352517a94319726fa68d8a0d609909bcb656470
Matt <matthias.calatroni@epfl.ch>
2025-04-25 16:43:55 +0200

    constructeur de ForceCentrale

--- exerciceP9.cc
+++ exerciceP9.cc
@@ -4,7 +4,11 @@
 #include "ChampForces.h"
 #include "ForceCentrale.h"
 #include "systeme.h"
+#include "PointMateriel.h"
 
 int main(){
-    Systeme s()
+    PointMateriel pomme({0,0,0},{0,0,0},0.1);
+    PointMateriel terre({0,0,6371.01e3},{0,0,0},5.972e24);
+    ChampNewtonien champ(terre);
+    Systeme sys({std::make_unique<PointMateriel>(pomme), std::make_unique<PointMateriel>(terre)});
 }
