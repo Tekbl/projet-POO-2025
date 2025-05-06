dd77d66e6db8ca84052cb6e351b1a47df42223e5
Matt <matthias.calatroni@epfl.ch>
2025-05-06 10:32:46 +0200

    force centrale

--- ForceCentrale.cc
+++ ForceCentrale.cc
@@ -4,8 +4,9 @@
 #include <vector>
 #include <iostream>
 #include "ObjetIntegrable.h"
+#include "ForceCentrale.h"
 
-class ForceCentrale: public ChampForces{
+/*class ForceCentrale: public ChampForces{
     protected:
         ForceCentrale(ObjetPhysique& obj): centre(obj){} 
         Vecteur quadratique_inverse(const ObjetPhysique& obj, double precis = 1e-10)const{
@@ -17,9 +18,18 @@ class ForceCentrale: public ChampForces{
             return (~r) * (1/ (norme * norme));
         }
         ObjetPhysique& centre;
-};
+};*/
 
-class ChampNewtonien: public ForceCentrale{
+Vecteur ForceCentrale::quadratique_inverse(const ObjetPhysique& obj, double precis = 1e-10) const{
+    Vecteur r = obj.get_E() - centre.get_E();
+    double norme = r.norme();
+    if (norme < precis){
+         return Vecteur(0,0,0);
+    }
+    return (~r) * (1/ (norme * norme));
+}
+
+/*class ChampNewtonien: public ForceCentrale{
     public:
     ChampNewtonien(ObjetPhysique& obj): ForceCentrale(obj){}
     virtual Vecteur force(const ObjetPhysique& obj, double t) const override{
@@ -31,4 +41,4 @@ class ChampNewtonien: public ForceCentrale{
     virtual void affiche(std::ostream& out) const override{
         whoami(out);
     }
-};
+};*/
--- ForceCentrale.h
+++ ForceCentrale.h
@@ -8,15 +8,21 @@
 
 class ForceCentrale: public ChampForces{
     protected:
-        ForceCentrale(ObjetPhysique& obj);
-        Vecteur quadratique_inverse(const ObjetPhysique& obj, double precis = 1e-10)const;
-        ObjetPhysique& centre;
+    ForceCentrale(ObjetPhysique& obj): centre(obj){} 
+    Vecteur quadratique_inverse(const ObjetPhysique& obj, double precis = 1e-10)const;
+    ObjetPhysique& centre;
 };
 
 class ChampNewtonien: public ForceCentrale{
     public:
-    ChampNewtonien(ObjetPhysique& obj);
-    virtual Vecteur force(const ObjetPhysique& obj, double t) const override;
-    virtual void whoami(std::ostream& out) const override;
-    virtual void affiche(std::ostream& out) const override;
+    ChampNewtonien(ObjetPhysique& obj): ForceCentrale(obj){}
+    virtual Vecteur force(const ObjetPhysique& obj, double t) const override{
+        return quadratique_inverse(obj)*((-G)*obj.get_masse()*centre.get_masse());
+    }
+    virtual void whoami(std::ostream& out) const override{
+        out << "Champ Newtonien : " ;
+    }
+    virtual void affiche(std::ostream& out) const override{
+        whoami(out);
+    }
 };
