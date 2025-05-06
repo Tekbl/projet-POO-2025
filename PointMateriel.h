5092432082bca91baee179e8ec5825b94f957ace
Matt <matthias.calatroni@epfl.ch>
2025-05-06 10:39:24 +0200

    pontmateriel: modifié, extermalisé le constructeur

--- PointMateriel.h
+++ PointMateriel.h
@@ -10,20 +10,27 @@
 #include "SupportADessin.h"
  
 class PointMateriel: public ObjetPhysique, public Dessinable{
-public:
-    PointMateriel(Vecteur E = {0,0,0}, Vecteur E_pr = {0,0,0}, double masse = 0, unsigned int dim = 3, const std::vector<ChampForces*>& c = std::vector<ChampForces*>{},
-    const std::vector<Contrainte*>& ctr = std::vector<Contrainte*>{});
-    /*Vecteur get_pos(int i) const { return vecteur_position.get_coord(i); }
-    Vecteur get_vit(int i) const { return vecteur_vitesse.get_coord(i); }
-    void set_pos(std::vector<double> a);
-    void set_vit(std::vector<double> a);*/
-    virtual Vecteur evolution(double t) override;
-    virtual void affiche(std::ostream& sortie)const override;
-    virtual void dessine_sur(SupportADessin& support) override{ support.dessine(*this); } //a copier coller dans toutes les sous classes de dessinable
-    virtual void whoami(std::ostream& out)const override;
+    public: 
+        PointMateriel(Vecteur E = {0,0,0}, Vecteur E_pr = {0,0,0}, double masse = 0, unsigned int dim = 3, const std::vector<ChampForces*>& c = std::vector<ChampForces*>{},
+            const std::vector<Contrainte*>& ctr = std::vector<Contrainte*>{}){};
+        /*double get_pos(int i) const {return vecteur_position.get_coord(i);};
+        double get_vit(int i) const {return vecteur_vitesse.get_coord(i);};
+        void set_pos(std::vector<double> a);
+        void set_vit(std::vector<double> a);*/
+        virtual Vecteur evolution(double t) override;
+        //Vecteur ChampForces = g; //instruction de la W4, à changer dans le futur 
 
-private:
-    /*Vecteur vecteur_position; 
-    Vecteur vecteur_vitesse;*/
+        virtual void affiche(std::ostream& sortie)const override;
+        virtual void dessine_sur(SupportADessin& support) override{ support.dessine(*this); } //a copier coller dans toutes les sous classes de dessinable
+        
+        virtual void whoami(std::ostream& out)const override{out << "point materiel" ;};
+
+        //constructeur ?
+        //destructeur ?
+
+    private:    
+        //Vecteur vecteur_position; //3D pour l'instant (vecteur etat dans l'exercice P5)
+        //Vecteur vecteur_vitesse; //3D et dérivée du vecteur position (vecteur dérivé du vecteur etat dans l'exercice P5)
+       
 };
 
