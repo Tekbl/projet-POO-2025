6e215c6564941cbad8c552bfa1dc98ef5a7444e7
Matt <matthias.calatroni@epfl.ch>
2025-05-06 10:28:07 +0200

    modif contrainte et contenu

--- contrainte.cc
+++ contrainte.cc
@@ -4,24 +4,25 @@
 #include "VecteurV.h"
 #include "ObjetIntegrable.h"
 #include "affichage.h"
+#include "contrainte.h"
 using namespace std;
 
 
-class Contrainte : public Printable{
+/*class Contrainte : public Printable{
     public:
         virtual Vecteur applique_force(const ObjetPhysique& obj, Vecteur force, double temps) = 0;
         virtual Vecteur position(const ObjetPhysique& obj) = 0;
         virtual Vecteur vitesse(const ObjetPhysique& obj) = 0;
-};
+};*/
 
-class Libre:public Contrainte{
+/*class Libre:public Contrainte{
     virtual Vecteur applique_force(const ObjetPhysique& obj, Vecteur force, double t) override;
     virtual Vecteur position(const ObjetPhysique& obj) override;
     virtual Vecteur vitesse(const ObjetPhysique& obj) override;
     virtual void whoami(ostream& out)const override{out << "contrainte libre" ;};
     virtual void affiche(std::ostream& out)const override{whoami(out);}//blank for now since we have no use for it
 
-};
+};*/
 
 Vecteur Libre::applique_force(const ObjetPhysique& obj, Vecteur force, double t){
     if(obj.get_masse()>0){
