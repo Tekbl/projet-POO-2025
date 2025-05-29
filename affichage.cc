5d195b835c6d898612295e635cf517ab20c71c01
Matt <matthias.calatroni@epfl.ch>
2025-05-29 19:08:17 +0200

    clean affichage.cc

--- affichage.cc
+++ affichage.cc
@@ -3,14 +3,6 @@
 
 using namespace std;
 
-/*class Printable{
-    public:
-        virtual void affiche(ostream& out) const = 0;
-        virtual ~Printable() = default;
-        virtual void whoami(ostream& out)const = 0;
-};*/
-
-
 ostream& operator<<(ostream& out, const Printable& print){
     print.affiche(out);
     return out;
