b6385bc062137bddbf62f23dcf631ca2c4620421
Matt <matthias.calatroni@epfl.ch>
2025-05-29 19:11:48 +0200

    commentaire sur constantes.cc modif de k, voir si problème sur exerciceP11-magnetique

--- constantes.cc
+++ constantes.cc
@@ -1,5 +1,5 @@
 #include "VecteurV.h"
 
-extern const Vecteur g(0.0,0.0,-9.81);
-extern const double k(8.99e10);
-extern const double G = 6.6743e-11; 
+extern const Vecteur g(0.0,0.0,-9.81); //vecteur gravité terrestre
+extern const double k(8.99e9); //constante de Coulomb
+extern const double G = 6.6743e-11; //constante de gravitation universelle
