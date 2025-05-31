a2a4ea78cdcf2584ed67142c5c51275152caf056
Matt <matthias.calatroni@epfl.ch>
2025-05-31 16:24:12 +0200

    commenté du code en plus dans les fichiers ForceCentrale.cc, ForceCentrale.h, ObjetDessinable.cc, exerciceP11-spherique.cc, exerciceP9-LeCorbeauEtLeRenard.cc et exerciceP9.cc

--- ForceCentrale.cc
+++ ForceCentrale.cc
@@ -7,11 +7,11 @@
 #include "ForceCentrale.h"
 
 Vecteur ForceCentrale::quadratique_inverse(const ObjetPhysique& obj, double precis) const{
-    Vecteur r = obj.get_E() - centre.get_E();
+    Vecteur r = obj.get_E() - centre.get_E(); //distance entre l'objet et le centre de la force centrale
     double norme = r.norme();
     if (norme < precis){
          return Vecteur(0,0,0);
     }
-    return (~r) * (1/ (norme * norme)); //    =  1/(r^2)
+    return (~r) * (1/ (norme * norme)); //    =  1/(r^2) * le vecteur unitaire de r pour avoir la direction de la force
 }
 
--- ObjetDessinable.cc
+++ ObjetDessinable.cc
@@ -1,12 +1,2 @@
 #include "ObjetDessinable.h"
 #include "SupportADessin.h"
-/*
-class Dessinable {
-
-    public :
-        virtual void dessine_sur(SupportADessin& support)=0;//a copier coller dans toutes les sous classes de dessinable
-    private : 
-    
-};
-
-*/
